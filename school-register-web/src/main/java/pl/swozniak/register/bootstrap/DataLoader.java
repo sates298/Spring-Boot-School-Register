146e76624f3adfcfe60e491f4004c2268d4e9334
sates298 <stanislaw.wo@interia.pl>
2019-08-07 14:54:36 +0200

    quick changes

--- school-register-web/src/main/java/pl/swozniak/register/bootstrap/DataLoader.java
+++ school-register-web/src/main/java/pl/swozniak/register/bootstrap/DataLoader.java
@@ -3,10 +3,16 @@ package pl.swozniak.register.bootstrap;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import pl.swozniak.register.dtos.GradeDTO;
+import pl.swozniak.register.dtos.ParentDTO;
+import pl.swozniak.register.dtos.SchoolClassDTO;
+import pl.swozniak.register.dtos.StudentDTO;
 import pl.swozniak.register.model.*;
 import pl.swozniak.register.model.enums.ClassLevel;
 import pl.swozniak.register.model.enums.GradeValue;
 import pl.swozniak.register.services.GradeService;
+import pl.swozniak.register.services.ParentService;
 import pl.swozniak.register.services.SchoolClassService;
 import pl.swozniak.register.services.StudentService;
 
@@ -19,11 +25,13 @@ public class DataLoader implements CommandLineRunner {
     private final StudentService studentService;
     private final SchoolClassService schoolClassService;
     private final GradeService gradeService;
+    private final ParentService parentService;
 
-    public DataLoader(StudentService studentService, SchoolClassService schoolClassService, GradeService gradeService) {
+    public DataLoader(StudentService studentService, SchoolClassService schoolClassService, GradeService gradeService, ParentService parentService) {
         this.studentService = studentService;
         this.schoolClassService = schoolClassService;
         this.gradeService = gradeService;
+        this.parentService = parentService;
     }
 
     @Override
@@ -36,6 +44,10 @@ public class DataLoader implements CommandLineRunner {
     }
 
     private void loadData(){
+        Parent parent = createParent(1L, "firstN", "lastN", "mail@mail.com", "123456789");
+
+        ParentDTO parentDto = saveParent(parent);
+
         Student student1 = createStudent(1L, "first1", "last1");
         Student student2 = createStudent(2L, "first2", "last2");
 
@@ -47,15 +59,19 @@ public class DataLoader implements CommandLineRunner {
         Grade grade4 = crateGrade(2L, GradeValue.FOUR, 2, "exam4");
 
 
-//        classA = saveSchoolClass(classA);
-//        classB = saveSchoolClass(classB);
-//        log.info("save classes");
+        SchoolClassDTO classAdto = saveSchoolClass(classA);
+        SchoolClassDTO classBdto = saveSchoolClass(classB);
+        log.info("save classes");
 
         student1.setSchoolClass(classA);
         student2.setSchoolClass(classA);
 
-        student1 = saveStudent(student1);
-        student2 = saveStudent(student2);
+        student1.setParent(parent);
+        student2.setParent(parent);
+
+        StudentDTO studentdto = saveStudent(student1);
+        StudentDTO student2dto = saveStudent(student2);
+
         log.info("save students");
 
         grade3.setStudent(student1);
@@ -73,6 +89,16 @@ public class DataLoader implements CommandLineRunner {
                 .id(id)
                 .level(level)
                 .students(new ArrayList<>())
+                .build();
+    }
+
+    private Parent createParent(long id, String first, String last, String email, String telephone) {
+        return Parent.builder()
+                .id(id)
+                .firstName(first)
+                .lastName(last)
+                .email(email)
+                .telephone(telephone)
                 .build();
     }
 
@@ -94,16 +120,20 @@ public class DataLoader implements CommandLineRunner {
                 .build();
     }
 
-    private Student saveStudent(Student student){
+    private StudentDTO saveStudent(Student student){
         return studentService.save(student);
     }
 
-//    private SchoolClass saveSchoolClass(SchoolClass schoolClass){
-//        return schoolClassService.save(schoolClass);
-//    }
+    private SchoolClassDTO saveSchoolClass(SchoolClass schoolClass){
+        return schoolClassService.save(schoolClass);
+    }
 
-    private Grade saveGrade(Grade grade){
+    private GradeDTO saveGrade(Grade grade){
         return gradeService.save(grade);
+    }
+
+    private ParentDTO saveParent(Parent parent){
+        return parentService.save(parent);
     }
 
 }
