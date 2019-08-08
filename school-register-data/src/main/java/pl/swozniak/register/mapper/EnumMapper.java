21dfd9383d5337a90b10a72038007173800434bc
sates298 <stanislaw.wo@interia.pl>
2019-08-08 15:18:16 +0200

    add EnumMapper to Spring

--- school-register-data/src/main/java/pl/swozniak/register/mapper/EnumMapper.java
+++ school-register-data/src/main/java/pl/swozniak/register/mapper/EnumMapper.java
@@ -4,6 +4,7 @@ import org.mapstruct.MapperConfig;
 import org.springframework.stereotype.Component;
 
 @MapperConfig(componentModel = "spring")
+@Component
 public class EnumMapper {
     protected String mapEnum(Enum<?> e) {
         if(e == null) return null;
