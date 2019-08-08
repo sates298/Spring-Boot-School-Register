3324fed9cddb979ae0765accdc4c95d4a9683420
sates298 <stanislaw.wo@interia.pl>
2019-08-08 15:27:47 +0200

    add Mapping in mappers

--- school-register-data/src/main/java/pl/swozniak/register/mapper/StudentMapper.java
+++ school-register-data/src/main/java/pl/swozniak/register/mapper/StudentMapper.java
@@ -1,10 +1,12 @@
 package pl.swozniak.register.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import pl.swozniak.register.dtos.StudentDTO;
 import pl.swozniak.register.model.Student;
 
 @Mapper(componentModel = "spring", uses = EnumMapper.class)
 public interface StudentMapper {
+    @Mapping(target = "schoolClass.students", ignore = true)
     StudentDTO studentToStudentDTO(Student student);
 }
